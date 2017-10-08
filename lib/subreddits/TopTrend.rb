dd5e61e88bdeb1c601d7f3b70edf1360dc2056c6
Sophia H <shhubeny@gmail.com>
2017-10-08 19:18:34 +0000

    got the first part of my cli to work

--- lib/subreddits/TopTrend.rb
+++ lib/subreddits/TopTrend.rb
@@ -6,7 +6,7 @@ class Subreddits::TopTrend
       @name = name
       @url = url
       @@all << self
-      @get_subreddit = Nokogiri::HTML(open("https://reddit.com/r/#{name}"))
+
     end
 
     def self.all
@@ -14,11 +14,13 @@ class Subreddits::TopTrend
     end
 
     def total_users(subreddit)
-      @get_subreddit.css('.side .subscribers .number').text
+      get_subreddit = Nokogiri::HTML(open("https://reddit.com/r/#{subreddit}"))
+      get_subreddit.css('.side .subscribers .number').text
     end
 
     def online_users(subreddit)
-      @get_subreddit.css('.side .users-online .number').text
+      get_subreddit = Nokogiri::HTML(open("https://reddit.com/r/#{subreddit}"))
+      get_subreddit.css('.side .users-online .number').text
     end
 
 
