d6aee3a4a114badcd7298752965d91eebc4e5324
Sophia H <shhubeny@gmail.com>
2017-10-08 15:29:34 +0000

    removed CLI function and fixed two other functions

--- lib/subreddits/cli.rb
+++ lib/subreddits/cli.rb
@@ -5,7 +5,6 @@ class Subreddits::CLI
     puts "Today's Trending Subreddits:--------------------------------------------------"
     display_subreddits
     retrieve_info
-    again
     goodbye
   end
 
@@ -14,20 +13,19 @@ class Subreddits::CLI
     input = gets.chomp
     while input != 'exit'
       #takes input, gets the subreddit associated with it, then does find_by_subreddit
-    puts "Name: #{Subreddits::TopTrend(input).name}"
-    puts "Total Subscribers: #{Subreddits::TopTrend(input).total}"
-    puts "Subscribers Online: #{Subreddits::TopTrend(input).online}"
-    puts "Subreddit rules: #{Subreddits::TopTrend(input).rules}"
-    puts "URL: #{Subreddits::TopTrend(input).url}"
-  end
-
-  def again?
-    puts ""
+      puts "Name: #{Subreddits::TopTrend(input).name}"
+      puts "Total Subscribers: #{Subreddits::TopTrend(input).total}"
+      puts "Subscribers Online: #{Subreddits::TopTrend(input).online}"
+      puts "Subreddit rules: #{Subreddits::TopTrend(input).rules}"
+      puts "URL: #{Subreddits::TopTrend(input).url}"
+      retrieve_info
+    end
   end
 
   def display_subreddits
     Subreddits::TopTrend.all.each_with_index do |sub, i|
       puts "#{i+1}. #{sub.name} | #{sub.karma}"
+    end
   end
 
   def goodbye
